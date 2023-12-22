de059e5b02531594e2537fc6b76ca11641da757d
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-22 11:12:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const parseCode = (str) => {
   // your code here
+	let ans={};
+	let i=0;
+	let temp='';
+	while(str[i]!=='0')temp+=str[i++];
+	ans.firstName=temp;
+	while(str[i]==='0')i++;
+	temp='';
+	while(str[i]!=='0')temp+=str[i++];
+	ans.lastName=temp;
+	while(str[i]==='0')i++;
+	temp='';
+	while(i<str.length)temp+=str[i++];
+	ans.id=temp;
+	return ans;
 };
 
 // Do not change the code below
